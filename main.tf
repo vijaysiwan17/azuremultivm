39fb4518192d1899f33d65a22e63270000510c70
vijay <vijaysiwan17@gmail.com>
2024-06-12 13:18:56 +0300

    vnet with tfstate

--- main.tf
+++ main.tf
@@ -1,67 +1,79 @@
 variable "virtual_network" {
-    type = object({
+  type = object({
     address_space = list(string)
-    name = string
 
-    })
-  
+
+  })
+  default = {
+    address_space = ["10.0.0.0/16",]
+  }
+
+}
+variable "tag" {
+type = map(string) 
+  default = {
+    "Name" = "project1"
+    "Environment" = "project1"
+  }
 }
 
-variable "env"{
-    type = string
-    default = "project1"
-    description = "Environment name"
+variable "env" {
+  type        = string
+  default     = "project1"
+  description = "Environment name"
 
 }
 terraform {
   required_providers {
     azurerm = {
-        source  = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "3.27.0"
     }
   }
 }
 resource "azurerm_network_security_group" "nsg" {
-    name = "${var.env}-nsg"
-    location = azurerm_resource_group.myrg.location
-    resource_group_name = azurerm_resource_group.myrg.name
-    security_rule {
-        name = "allow_ssh"
-        priority = 100
-        direction = "Inbound"
-        access = "Allow"
-        protocol = "Tcp"
-        source_port_range = "*"
-        destination_port_range = "22"
-        source_address_prefix = "*"
-        destination_address_prefix = "*"
-    }
-  
+    tags = var.tag
+  name                = "${var.env}-nsg"
+  location            = azurerm_resource_group.myrg.location
+  resource_group_name = azurerm_resource_group.myrg.name
+  security_rule {
+    name                       = "allow_ssh"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
 }
 resource "azurerm_resource_group" "myrg" {
-
-    name = "${var.env}-rg"
-    location = "eastus"
+tags = var.tag
+  name     = "${var.env}-rg"
+  location = "eastus"
 }
 
 provider "azurerm" {
-    features {
+  features {
 
-    }
+  }
 }
 resource "azurerm_virtual_network" "vnet" {
-    resource_group_name = azurerm_resource_group.myrg.name
-    location = azurerm_resource_group.myrg.location
-    name = "${var.env}-vnet"
-    address_space = ["10.0.0.0/16"]
-    subnet {
-        name = "${var.env}-subnet1"
-        address_prefix = "10.0.0.0/24"
-        security_group = azurerm_network_security_group.nsg.id
-    }
+    tags = var.tag
+  resource_group_name = azurerm_resource_group.myrg.name
+  location            = azurerm_resource_group.myrg.location
+  name                = "${var.env}-vnet"
+  address_space       = var.virtual_network.address_space
+  subnet {
+    name           = "${var.env}-subnet1"
+    address_prefix = "10.0.0.0/24"
+    security_group = azurerm_network_security_group.nsg.id
+  }
 
 
-    }
+}
 
-  
+
 
